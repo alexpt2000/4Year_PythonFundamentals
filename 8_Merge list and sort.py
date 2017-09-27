9655c3e616e2e58000227f928290b55a14f8e05d
Alexander Souza <g00317835@gmit.ie>
2017-09-27 10:21:01 +0100

    Merge Sort List

--- 8_Merge list and sort.py
+++ 8_Merge list and sort.py
@@ -1,4 +1,4 @@
-def merge (l1,l2):
+def mergeSort (l1,l2):
     if len(l1) == 0 and len(l2) == 0:
         return []
     if len(l1) == 0:
@@ -6,12 +6,12 @@ def merge (l1,l2):
     if len(l2) == 0:
         return l1
     if l1[0] <= l2[0]:
-        return [l1[0]] + merge(l1[1:], l2)
+        return [l1[0]] + mergeSort(l1[1:], l2)
     if l2[0] <= l1[0]:
-        return [l2[0]] + merge(l1, l2[1:])
+        return [l2[0]] + mergeSort(l1, l2[1:])
 
 
 l1 = [1, 3, 4, 7]
 l2 = [0, 2, 5, 6, 8, 9]
 
-print(merge(l1+l2))
+print(mergeSort(l1, l2))
